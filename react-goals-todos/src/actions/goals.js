53b07ad90bfac618ac1f60d19389a8cfac36a22c
sujitbidawe <bidawesujit@gmail.com>
2022-09-13 17:45:35 +0530

    RESOLVED: goal input not cleared after adding goal issue

--- react-goals-todos/src/actions/goals.js
+++ react-goals-todos/src/actions/goals.js
@@ -21,6 +21,7 @@ export function handleAddGoal(name, cb) {
     return (dispatch) => {
         return API.saveGoal(name).then((goal) => {
             dispatch(addGoal(goal));
+            cb();
         }).catch(() => {
             alert("There was an error. Please try again.");
         })
